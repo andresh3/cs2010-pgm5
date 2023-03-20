2ca92b458a5ee61e1166d90358697012aa007097
Andrew Humphreys <andrew@hmphreys.cc>
2023-03-20 16:17:46 -0400

    Refactor variable names for consistency

--- HumphreysA_pgm5.cpp
+++ HumphreysA_pgm5.cpp
@@ -24,13 +24,13 @@ int main()
 {
 	// Variables
 	ifstream in_fs;
-	string realtor_names[MAX_REALTORS];
+	string realtor_names_arr[MAX_REALTORS];
 	int realtor_sales_arr[MAX_REALTORS];
 
-	int realtor_most_sales;
+	int top_realtor;
 	int most_sales;
 
-	int realtor_least_sales;;
+	int bottom_realtor;;
 	int least_sales;
 
 	int total_sales = 0;
@@ -50,7 +50,7 @@ int main()
 	{
 		int raw_realtor_sales;
 
-		in_fs >> realtor_names[i];
+		in_fs >> realtor_names_arr[i];
 		in_fs >> raw_realtor_sales;
 
 		realtor_sales_arr[i] = abs(raw_realtor_sales);
@@ -64,37 +64,37 @@ int main()
 	in_fs.close();
 	// Print each realtor, and then find the highest sales
 
-	realtor_most_sales = 0;
-	most_sales = realtor_sales_arr[realtor_most_sales];
+	top_realtor = 0;
+	most_sales = realtor_sales_arr[top_realtor];
 
-	realtor_least_sales = 0;
-	least_sales = realtor_sales_arr[realtor_least_sales];
+	bottom_realtor = 0;
+	least_sales = realtor_sales_arr[bottom_realtor];
 
-	int realtor_number;
+	int realtor_id;
 	int curr_realtor_sales;
 
 	// Preform processing on data:
 	for (int i = 0;i < MAX_REALTORS;i++)
 	{
 		// Alternative condition to stop execution
-		bool realtor_exists = realtor_names[i].empty();
+		bool realtor_exists = realtor_names_arr[i].empty();
 		if (realtor_exists)
 			break;
 
-		realtor_number = i + 1;
-		cout << "Realtor " << realtor_number << ", " << realtor_names[i] << ", sold " << realtor_sales_arr[i] << " houses." << endl;
+		realtor_id = i + 1;
+		cout << "Realtor " << realtor_id << ", " << realtor_names_arr[i] << ", sold " << realtor_sales_arr[i] << " houses." << endl;
 
 		// Calculate max, min and last total
 		curr_realtor_sales = realtor_sales_arr[i];
 		if (curr_realtor_sales > most_sales)
 		{
-			realtor_most_sales = i;
+			top_realtor = i;
 			most_sales = curr_realtor_sales;
 		}
 
 		if (curr_realtor_sales < least_sales)
 		{
-			realtor_least_sales = i;
+			bottom_realtor = i;
 			least_sales = curr_realtor_sales;
 		}
 
@@ -104,11 +104,13 @@ int main()
 
 
 	// Last provide program output.
-	realtor_number = realtor_most_sales + 1;
-	cout << "The winner is Realtor " << realtor_number << ", " << realtor_names[realtor_most_sales] << ", who sold " << most_sales << " houses." << endl;
+	realtor_id = top_realtor + 1;
+	string top_realtor_name = realtor_names_arr[top_realtor];
+	cout << "The winner is Realtor " << realtor_id << ", " << top_realtor_name << ", who sold " << most_sales << " houses." << endl;
 
-	realtor_number = realtor_least_sales + 1;
-	cout << "Realtor " << realtor_number << ", " << realtor_names[realtor_least_sales] << ", sold the least number of houses: " << least_sales << endl;
+	realtor_id = bottom_realtor + 1;
+	string bottom_realtor_name = realtor_names_arr[bottom_realtor];
+	cout << "Realtor " << realtor_id << ", " << bottom_realtor_name << ", sold the least number of houses: " << least_sales << endl;
 
 	cout << "And in total all Realtors sold " << total_sales << " houses." << endl;
 	
